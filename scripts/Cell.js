a2d41e0ff4f269507483941f01d307072d9a448f
Michael Kolesidis <michael.kolesidis@gmail.com>
2021-08-31 20:25:06 +0300

    Add files via upload

---
+++ scripts/Cell.js
@@ -0,0 +1,30 @@
+class Cell {
+	constructor(i, j) {
+		this.i = i;
+		this.j = j;
+		this.x = i * cellW;
+		this.y = j * cellH;
+		this.mine = false;
+		this.minesAround = 0;
+		this.revealed = false;
+		this.won = false;
+	}
+
+	draw() {
+		if (this.revealed && this.mine) {
+			text(MINE, this.x, this.y);
+			return;
+		}
+		if (this.revealed) {
+			// Calculate the number of mines around and draw that
+			text(DIGITS[this.minesAround], this.x, this.y);
+			return;
+		}
+		if (this.flagged) {
+			text(FLAG, this.x, this.y);
+			return;
+		}
+
+		text(EMPTY, this.x, this.y);
+	}
+}
